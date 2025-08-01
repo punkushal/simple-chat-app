20f259b46175884004cba610ba5e6ccf942731c1
punkushal <spycymagar@gmail.com>
2025-08-01 07:39:31 +0545

    necessary property added in custom button

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, required this.buttonLabelText});
+  const CustomButton({
+    super.key,
+    required this.buttonLabelText,
+    this.onPressed,
+    required this.isLoading,
+    this.color,
+  });
   final String buttonLabelText;
   final void Function()? onPressed;
+  final bool isLoading;
+  final Color? color;
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(onPressed: onPressed, child: Text(buttonLabelText));
+    return SizedBox(
+      width: double.infinity,
+      height: 50,
+      child: ElevatedButton(
+        onPressed: isLoading ? null : onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: color ?? Theme.of(context).primaryColor,
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadiusGeometry.circular(8),
+          ),
+        ),
+        child: Text(buttonLabelText),
+      ),
+    );
   }
 }
