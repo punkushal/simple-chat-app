5edfe7ed76be606dbdb09e5b56d1d4abfef78c38
punkushal <spycymagar@gmail.com>
2025-08-07 06:41:25 +0545

    expanded widget added

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:simple_chat_app/models/user_model.dart';
 import 'package:simple_chat_app/providers/auth_provider.dart';
 import 'package:simple_chat_app/providers/chat_provider.dart';
+import 'package:simple_chat_app/widgets/message_bubble.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key, required this.chatUser});
@@ -13,6 +14,7 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   final _messageController = TextEditingController();
+  final _scrollController = ScrollController();
 
   @override
   void initState() {
@@ -34,12 +36,25 @@ class _ChatScreenState extends State<ChatScreen> {
   void dispose() {
     super.dispose();
     _messageController.dispose();
+    _scrollController.dispose();
+  }
+
+  void _scrollToBottm() {
+    if (_scrollController.hasClients) {
+      _scrollController.animateTo(
+        _scrollController.position.maxScrollExtent,
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeOut,
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
+        backgroundColor: Colors.white,
         title: Row(
           children: [
             CircleAvatar(
@@ -76,6 +91,121 @@ class _ChatScreenState extends State<ChatScreen> {
           ],
         ),
       ),
+      body: Column(
+        children: [
+          Consumer2<ChatProvider, AuthProvider>(
+            builder: (context, chatProvider, authProvider, child) {
+              if (chatProvider.isLoading) {
+                return Center(child: CircularProgressIndicator());
+              } else if (chatProvider.errorMessage.isNotEmpty) {
+                return Center(
+                  child: Text(
+                    'Error: ${chatProvider.errorMessage}',
+                    style: TextStyle(color: Colors.red),
+                  ),
+                );
+              } else if (chatProvider.messages.isEmpty) {
+                return Center(child: Text('No chatting started yet!!'));
+              }
+              WidgetsBinding.instance.addPostFrameCallback(
+                (_) => _scrollToBottm(),
+              );
+              return Expanded(
+                child: ListView.builder(
+                  controller: _scrollController,
+                  itemCount: chatProvider.messages.length,
+                  itemBuilder: (ctx, index) {
+                    final message = chatProvider.messages[index];
+                    final isCurrentUser =
+                        message.senderId == authProvider.currentUser!.uid;
+
+                    return MessageBubble(
+                      message: message,
+                      isCurrentUser: isCurrentUser,
+                    );
+                  },
+                ),
+              );
+            },
+          ),
+          _buildMessageInput(),
+        ],
+      ),
     );
+  }
+
+  Widget _buildMessageInput() {
+    return Container(
+      padding: EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, -2),
+            blurRadius: 6,
+            color: Colors.black.withValues(alpha: 0.1),
+          ),
+        ],
+      ),
+      child: Row(
+        spacing: 6,
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _messageController,
+              decoration: InputDecoration(
+                hintText: 'Type a message...',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(24),
+                ),
+              ),
+              maxLines: null,
+              textCapitalization: TextCapitalization.sentences,
+            ),
+          ),
+          Consumer2<ChatProvider, AuthProvider>(
+            builder: (context, chatProvider, authProvider, child) {
+              return CircleAvatar(
+                backgroundColor: Theme.of(context).primaryColor,
+
+                child: IconButton(
+                  onPressed: chatProvider.isSending
+                      ? null
+                      : () => _sendMessage(chatProvider, authProvider),
+                  icon: chatProvider.isSending
+                      ? SizedBox(
+                          height: 20,
+                          width: 20,
+                          child: CircularProgressIndicator(
+                            strokeWidth: 2,
+                            valueColor: AlwaysStoppedAnimation(Colors.white),
+                          ),
+                        )
+                      : Icon(Icons.send, color: Colors.white),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _sendMessage(
+    ChatProvider chatProvider,
+    AuthProvider authProvider,
+  ) async {
+    final messageText = _messageController.text.trim();
+
+    if (messageText.isEmpty || authProvider.currentUser == null) {
+      return;
+    }
+    await chatProvider.sendMessage(
+      senderId: authProvider.currentUser!.uid,
+      receiverId: widget.chatUser.uid,
+      message: messageText,
+      senderName: authProvider.currentUser!.name,
+    );
+    _scrollToBottm();
   }
 }
